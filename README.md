53c512bd21dc9491c01595c202f247b4b3cc78f2
bhu1-103 <103.bhuvan@gmail.com>
2026-08-18 01:15:48 +0530

    made download models script more colorful and updated readme

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # desktop-assistant
 
+## specs
+
 - C++ based
 - **wakeword detection** -> [lowwi](https://github.com/CLFML/lowwi)
 - ~**wakeword trainer** -> [dscripka's trainer](https://github.com/dscripka/openWakeWord) with [dscripka's synthetic data generator](https://github.com/dscripka/piper-sample-generator)~
-- **wakeword trainer** -> [livekit's trainer](https://github.com/livekit/livekit-wakeword) -> 
+- **wakeword trainer** -> [livekit's trainer](https://github.com/livekit/livekit-wakeword)
+
+## how to build
+
+- clone the repo -> `git clone --recurse-submodules https://github.com/bhu1-103/desktop-assistant.git`
+- move into the directory -> `cd desktop-assistant`
+- download the required models -> `./scripts/download-models.sh`
+- run the build command -> `./scripts/build.sh`
+- run the program -> `./build/friday`
--- scripts/download-models.sh
+++ scripts/download-models.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/sh
 
-set -e
+set -e #for reroll
 mkdir -p models
 
-echo "Syncing friday.onnx, melspectrogram.onnx and embedding_model.onnx"
-curl -L --fail -o models/friday.onnx https://github.com/bhu1-103/desktop-assistant/releases/download/v-0.1/friday.onnx
-curl -L --fail -o models/melspectrogram.onnx https://raw.githubusercontent.com/CLFML/lowwi/46409651a9ed78912167ce3f24e7d5df97e24c81/models/melspectrogram.onnx
-curl -L --fail -o models/embedding_model.onnx https://raw.githubusercontent.com/CLFML/lowwi/46409651a9ed78912167ce3f24e7d5df97e24c81/models/embedding_model.onnx
-echo "Done syncing models"
+RESET='\033[0m'
+YELLOW='\033[93m'
+GREEN='\033[32m'
+CYAN='\033[36m'
+
+echo -e "${GREEN}Syncing friday.onnx${RESET}"
+curl -sSL --fail -o models/friday.onnx https://github.com/bhu1-103/desktop-assistant/releases/download/v-0.1/friday.onnx
+echo -e "${GREEN}Syncing melspectrogram.onnx${RESET}"
+curl -sSL --fail -o models/melspectrogram.onnx https://raw.githubusercontent.com/CLFML/lowwi/46409651a9ed78912167ce3f24e7d5df97e24c81/models/melspectrogram.onnx
+echo -e "${GREEN}Syncing embedding_model.onnx${RESET}"
+curl -sSL --fail -o models/embedding_model.onnx https://raw.githubusercontent.com/CLFML/lowwi/46409651a9ed78912167ce3f24e7d5df97e24c81/models/embedding_model.onnx
+echo -e "${CYAN}Done syncing models${RESET}"
+echo -e "${YELLOW}Run ./scripts/build.sh now${RESET}"
