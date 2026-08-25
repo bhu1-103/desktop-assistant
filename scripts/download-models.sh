ae25dae1518692822ef3bb77703a459e603db7a1
bhu1-103 <103.bhuvan@gmail.com>
2026-08-26 01:06:52 +0530

    dl script done

--- scripts/download-models.sh
+++ scripts/download-models.sh
@@ -14,9 +14,9 @@ echo -e "${GREEN}Syncing melspectrogram.onnx${RESET}"
 curl -sSL --fail -o models/melspectrogram.onnx https://raw.githubusercontent.com/CLFML/lowwi/46409651a9ed78912167ce3f24e7d5df97e24c81/models/melspectrogram.onnx
 echo -e "${GREEN}Syncing embedding_model.onnx${RESET}"
 curl -sSL --fail -o models/embedding_model.onnx https://raw.githubusercontent.com/CLFML/lowwi/46409651a9ed78912167ce3f24e7d5df97e24c81/models/embedding_model.onnx
-echo -e "${CYAN}Done syncing models${RESET}"
-echo -e "${YELLOW}Run ./scripts/build.sh now${RESET}"
 echo -e "${GREEN}Syncing ggml-tiny.en-q5_1.bin${RESET}"
 wget -P models/ https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-tiny.en-q5_1.bin
 echo -e "${GREEN}Syncing music-controls_embeddings.lua ${RESET}"
 curl -sSL --fail -o models/friday.onnx https://github.com/bhu1-103/desktop-assistant/releases/download/v-0.1/music-controls_embeddings.lua
+echo -e "${CYAN}Done syncing models${RESET}"
+echo -e "${YELLOW}Run ./scripts/build.sh now${RESET}"
